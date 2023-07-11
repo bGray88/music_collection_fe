00013ec179ba331e62e7ef8e812b532084daf1e2
Brandon Gray <brandonjoe88@hotmail.com>
2023-07-11 10:40:13 -0600

    feat: adds album addition to collection functionality

--- src/components/container/AlbumsContainer.js
+++ src/components/container/AlbumsContainer.js
@@ -1,54 +1,99 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class AlbumsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      albums: [],
-    };
-  }
+const AlbumsContainer = (props) => {
+  const [albums, setAlbums] = useState([]);
+  const [artistId, setArtistId] = useState('');
 
-  loadAlbums() {
+  const loadAlbums = () => {
     axios
       .get("/api/v1/albums")
       .then((res) => {
-        this.setState({ albums: res.data.data });
+        setAlbums(res.data.data);
       })
       .catch((error) => console.log(error));
   }
 
-  componentDidMount() {
-    this.loadAlbums();
+  const onFormSubmit = async (e) => {
+    e.preventDefault();
+    const artistName = e.target[0].value;
+    const artistYear = e.target[1].value;
+    const albumTitle = e.target[2].value;
+    const albumYear  = e.target[3].value;
+    const albumGenre = e.target[4].value;
+    await axios
+      .post("/api/v1/artists", {
+        artist: {
+          name: artistName,
+          form_year: artistYear
+      }})
+      .then((res) => {
+        setArtistId(res.data.artist_id);
+        console.log(artistId);
+      })
+      .catch((error) => console.log(error));
+    await axios
+      .post("/api/v1/albums", {
+        album: {
+          title: albumTitle,
+          release_year: albumYear,
+          genre: albumGenre,
+          artist_id: artistId
+      }})
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => console.log(error));
   }
 
-  render() {
-    return (
-      <div>
-        <div className="taskContainer">
-          <input
-            className="newTask"
-            type="text"
-            placeholder="Input a New Album and Press Enter"
-            maxLength="75"
-          />
-        </div>
-        <div className="wrapItems">
-          <ul className="listItems">
-            {this.state.albums.map((album) => {
-              return (
-                <li className="item" album={album} key={album.id}>
-                  <input className="itemCheckbox" type="checkbox" />
-                  <label className="itemDisplay">{album.attributes.title}</label>
-                  <span className="removeItemButton">x</span>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+  useEffect(() => {
+    loadAlbums();
+  }, [])
+
+  return (
+    <div className="albumContainer">
+      <form className="newAlbumArtist"
+        onSubmit={onFormSubmit}
+      >
+        <input type="text"
+          className="albumArtist"
+          placeholder="Input Album Artist"
+          maxLength="75" />
+        <input type="text"
+          className="albumArtistFormYear"
+          placeholder="Input Artist's form year"
+          maxLength="75" />
+        <input type="text"
+          className="artistAlbum"
+          placeholder="Input Artist's Album"
+          maxLength="75" />
+        <input type="text"
+          className="artistAlbumReleaseYear"
+          placeholder="Input Album's release year"
+          maxLength="75" />
+        <input type="text"
+          className="artistAlbumGenre"
+          placeholder="Input Album's genre"
+          maxLength="75" />
+        <button type="submit"
+          className="btn btn-success"
+        >Submit</button>
+      </form>
+      <div className="wrapItems">
+        <ul className="listItems">
+          { albums.map ((album) => {
+            return (
+              <li className="item" album={album} key={album.id}>
+                <input className="itemCheckbox" type="checkbox" />
+                <label className="itemDisplay">{album.attributes.title}</label>
+                <span className="removeItemButton">x</span>
+              </li>
+            )
+          }) }
+        </ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default AlbumsContainer;
